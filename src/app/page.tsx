88515cf4217ea57764bbe5aaadaa776214217ee3
Adamovich <Ndptrv@yandex.by>
2023-08-14 21:52:59 +0300

    asdasd

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ const Home = () => {
   return (
     <main className="flex min-h-screen max-w-2xl flex-col items-center p-24">
       <Link href="/">Home</Link>
+
       <Link href="cv">CV</Link>
     </main>
   );
